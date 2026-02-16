d2b704d07ad5fa683ebc564cac3788a61cd760b7
Mr-burns <mr-burns@mr-burns-Nitro-ANV16-41>
2026-02-16 13:17:44 +0100

    bubble_sort

--- exercices_random/bubblesort.c
+++ exercices_random/bubblesort.c
@@ -1,40 +1,42 @@
-/*********************************************/
-/*   bubblesort.c*/
-/*                                           */
-/*   By: mr-burns*/
-/*   Created: 2025/09/05 22:08:12*/
-/*   Updated: 2025/09/05 22:08:12*/
-/*                                           */
-/*********************************************/
-void	bubblesort(int *str, int n)
+#include <stdio.h>
+
+void bubblesort(int *str, int n)
 {
 	int	i;
 	int	j;
-	int	temp;
+	int	tmp;
 	i = 0;
 	j = 0;
 
-	while (i != n - 1)
+	while (i < n - 1)
 	{
-		while(j < n - 1)
+		while (j < n - 1)
 		{
-	
 			if (str[j] > str[j + 1])
 			{
-				temp = str[j]; 
+				tmp = str[j];
 				str[j] = str[j + 1];
-				str[j + 1] = temp;
-		
-		j++;
+				str[j + 1] = tmp;
 			}
+			if (str[j] < str[j + 1])
+				j++;
+			else 
+				j++;
 		}
-	j = 0;
-	i++;
+		j = 0;
+		i++;
 	}
-		
+
+	i = 0;
+
+	while ( i < n)
+	{
+		printf("%d", str[i]);
+		i++;
+	} 
 }
 int main()
 {
-	int str[] = {5, 1, 6, 9, 3, 7, 4};
+	int str[] = {1, 3, 5 ,5 ,7 ,4, 6};
 	bubblesort(str, 7);
 }
---
+++ exercices_random/tes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int entier[] = {1, 2, 3, 4};
+	int i = 0;
+	
+		printf("%d\n", entier[1]);
+
+
+}
